9ca345ea64fd65d780a4307f8b8f2885b684bd6d
abdulquddus <qudus.yekeen@andela.com>
2018-05-24 11:40:54 +0100

    feat(Django-API): users and snippets endpoint using Django

---
+++ tutorial/snippets/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from pygments.lexers import get_all_lexers, get_lexer_by_name
+from pygments.styles import get_all_styles
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
+
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGES_CHOICES = sorted([(items[1][0], items[0]) for items in LEXERS])
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
+
+# Create your models here.
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGES_CHOICES, default='python', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField()
+    
+    class Meta:
+        ordering = ('created',)
+
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representtation of the code snippet.
+        """
+        lexers = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(
+            style=self.style, linenos=linenos, full=True, **options)
+        self.highlighted = highlight(self.code, lexers, formatter)
+        super(Snippet, self).save(*args, **kwargs)
---
+++ tutorial/snippets/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers, permissions
+from snippets.models import Snippet, LANGUAGES_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+
+    class Meta:
+        model =Snippet
+        fields = ('url', 'id', 'highlight', 'owner', 'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedIdentityField(
+                many=True, view_name='snippet-detail',  read_only=True
+            )
+    class Meta:
+        model = User
+        fields = ('id', 'url', 'username', 'snippets')
